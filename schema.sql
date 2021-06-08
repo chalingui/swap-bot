77547b38ebfe2da71418436fcec87eb7c79f28ae
Carlos Sesma <carlos.sesma@gmail.com>
2021-06-07 21:38:26 -0300

    Database schema

---
+++ schema.sql
@@ -0,0 +1,29 @@
+CREATE TABLE `logs` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `trade_pair` varchar(255) DEFAULT NULL,
+  `type` enum('buy','sell') DEFAULT NULL,
+  `amount` decimal(10,4) DEFAULT NULL,
+  `base_price` decimal(10,4) DEFAULT NULL,
+  `market_price` decimal(10,4) DEFAULT NULL,
+  `percent` int(11) DEFAULT NULL,
+  `estimated_gas` bigint(20) DEFAULT NULL,
+  `gas_price` bigint(20) DEFAULT NULL,
+  `action` enum('yes','no') DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `trades` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `log_id` int(11) DEFAULT NULL,
+  `type` enum('BUY','SELL') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `tx` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fromToken` varchar(4) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `fromTokenAmount` decimal(10,4) DEFAULT NULL,
+  `toToken` varchar(4) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `toTokenAmount` decimal(10,4) DEFAULT NULL,
+  `gas_used` bigint(20) DEFAULT NULL,
+  `gas_price` bigint(20) DEFAULT NULL,
+  `date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
